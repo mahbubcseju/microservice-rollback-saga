7313b0206c010b878cf53073d84a911cf4363a3b
Md. Mahbubur Rahman <mahbuburrahman2111@gmail.com>
2020-06-26 11:03:59 +0900

    Update const.js

--- const.js
+++ const.js
@@ -1,6 +1,6 @@
 var config = {
-    USER_SERVICE :  "https://sqs.ap-northeast-1.amazonaws.com/467064118795/USER_SERVICE",
-    ORDER_SERVICE : "https://sqs.ap-northeast-1.amazonaws.com/467064118795/ORDER_SERVICE",
+    USER_SERVICE :  *****
+    ORDER_SERVICE : ****
     ORDER_SERVER : "http://localhost:5002",
     USER_SERVER: "http://localhost:5001"
 }
